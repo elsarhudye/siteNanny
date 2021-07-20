c9bd2bcfedfaa2af539a4699a051c8cd3eaef7a5
Elsa Rhudye Mangoye <diamant.rhudye@gmail.com>
2021-07-20 11:33:53 +0200

    homepage

---
+++ src/Controller/NannyController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NannyController extends AbstractController
+{
+    #[Route('/', name: 'nanny_home')]
+    public function home(): Response
+    {
+        return $this->render('nanny/home.html.twig', [
+            'controller_name' => 'NannyController',
+        ]);
+    }
+}
